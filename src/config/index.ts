9bc405d72fdb003b71335ebc49d08951c572251f
jakkaphatCM <jakkaphat.m@pdkm.tech>
2025-07-09 14:07:57 +0700

    feat : auth :: jwt add generate token

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,5 @@
 import dotenv from 'dotenv';
+import type ms from 'ms';
 
 dotenv.config();
 
@@ -8,6 +9,10 @@ const config = {
   WHITELIST_ORIGINS: ['https://google.com'],
   MONGO_URI: process.env.MONGO_URI,
   LOG_LEVEL: process.env.LOG_LEVEL,
+  JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET!,
+  JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET!,
+  ACCESS_TOKEN_EXPIRY: process.env.ACCESS_TOKEN_EXPIRY as ms.StringValue,
+  REFRESH_TOKEN_EXPIRY: process.env.REFRESH_TOKEN_EXPIRY as ms.StringValue,
 };
 
 export default config;
