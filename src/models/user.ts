68c427459e20981a6dbb5442535fadce6519445f
jakkaphatCM <jakkaphat.m@pdkm.tech>
2025-07-09 14:58:39 +0700

    fix : user :: password not hash

--- src/models/user.ts
+++ src/models/user.ts
@@ -84,11 +84,10 @@ const userSchema = new Schema<IUser>(
   { timestamps: true },
 );
 
-// hash password
+// on create
 userSchema.pre('save', async function (next) {
-  if (this.isModified('password')) {
-    next();
-    return;
+  if (!this.isModified('password')) {
+    return next();
   }
 
   this.password = await bcrypt.hash(this.password, 10);
